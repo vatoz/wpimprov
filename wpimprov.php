aff53f7dd2a0f1e37302eb8ffd23b629dd31f9d7
Vaclav Cerny <vatoz@seznam.cz>
2016-09-30 11:35:28 +0200

    Better languages, events

--- wpimprov.php
+++ wpimprov.php
@@ -14,13 +14,14 @@ add_action( 'init', 'wpimprov_create_post_type' );
 
 /*
 Vytvoří taxonomii pro týmy a samotný typ zápisku tým
+Dále vytvoří event
 */
 function wpimprov_create_post_type() {
 	register_taxonomy(
 	'wpimprov_city',
 	'wpimprov_team',
 	array(
-			'label' => __( 'City' ),
+			'label' => __( 'City','wpimprov' ),
 			//'rewrite' => array( 'slug' => 'city' ),
 			'show_ui'           => true,
 		'show_admin_column' => true,
@@ -31,8 +32,8 @@ function wpimprov_create_post_type() {
 	register_post_type( 'wpimprov_team',
 		array(
 		'labels' => array(
-			'name' => __( 'Teams' ),
-			'singular_name' => __( 'Team' )
+			'name' => __( 'Improliga teams','wpimprov' ),
+			'singular_name' => __( 'Improliga team','wpimprov' )
 		),
 		'taxonomies'=>array(
 			'wpimprov_city'
@@ -46,6 +47,30 @@ function wpimprov_create_post_type() {
 		),
 		'public' => true,
 		'has_archive' => true,
+		'menu_icon' => 'dashicons-universal-access-alt',
+		'rewrite'  => array( 'slug' => 'team' ),
 		)
 	);
+	
+	register_post_type( 'wpimprov_event',
+		array(
+		'labels' => array(
+			'name' => __( 'Improliga events','wpimprov' ),
+			'singular_name' => __( 'Improliga event','wpimprov' )
+		),
+		"supports"=>array(
+			"title",
+			"editor",
+			"thumbnail",
+			"revisions",
+			"custom-fields",
+		),
+		'public' => true,
+		'has_archive' => true,
+		'menu_icon' => 'dashicons-calendar-alt',
+		'rewrite'  => array( 'slug' => 'event' ),
+		)
+	);	
+	
+	
 }
