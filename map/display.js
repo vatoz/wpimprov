42cb3f7bd0dd217df90fd3edae719eff6ecca285
Vaclav Cerny <vatoz@seznam.cz>
2017-01-27 13:06:04 +0100

    Buďme dál od mapy, při kliknutí na skupinu ji zobrazme

--- map/display.js
+++ map/display.js
@@ -1,11 +1,14 @@
-
 		var tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 				maxZoom: 18,
 				attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
 			}),
 			latlng = L.latLng(50.0, 15.00);
-
-		var map = L.map('map', {center: latlng, zoom: 7, layers: [tiles]});
+		if ( jQuery("#map").width()<400){
+			var map = L.map('map', {center: latlng, zoom: 5, layers: [tiles]});
+		}else{
+		var map = L.map('map', {center: latlng, zoom: 6, layers: [tiles]});
+		}
+		
 		var MyIcon = L.Icon.extend({
     options: {
 
@@ -23,13 +26,14 @@
 			var title = a[2];
 			
 			var ico=L.divIcon({
-			html:'<img src="'+a[3]+'" width=40px >',
+			html:'<a href="'+a[4]+'"><img src="'+a[3]+'" width=40px ></a>',
 			iconSize:[40, 40]
 			}) ;
 		
 			var marker = L.marker(new L.LatLng(a[0], a[1]), {icon: ico});
 			
-			marker.bindPopup('<a href="'+a[4]+'">'+title+'<br><img src="'+a[3]+'" width=80px></a>');
+	
+	   
 		  //marker.addTo(map);
 			 
 			markers.push(marker);
